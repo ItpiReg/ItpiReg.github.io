49a85dc6bb141950ffe8f4f9065019b86b789136
ItpiReg <sergei130887@gmailcom>
2020-05-12 00:20:22 +0300

    Initial commit

---
+++ mail.php.php
@@ -0,0 +1,40 @@
+<?php 
+
+require_once('https://firebasestorage.googleapis.com/v0/b/myapplication3-a67c0.appspot.com/o/mail%2Findex.html?alt=media&token=38a14d3c-0b73-418e-b956-83bf894f00db');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'message_mail_website@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '$357vyyss'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('message_mail_website@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('message_mail_website@mail.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: https://drive.google.com/open?id=1MCqTb4lzozlGb0hfnlSXinWDdbHOUN7j');
+}
+?>
